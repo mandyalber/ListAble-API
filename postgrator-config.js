50d10d78ae2cbab7a92c35417d65b310710b0fe9
Mandy Alber <mandy.alber@gmail.com>
2021-04-01 11:12:42 -0700

    test deployment

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,4 +6,7 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
+    ssl: {
+    rejectUnauthorized: false
+  }
 }
